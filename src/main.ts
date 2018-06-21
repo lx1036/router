23692bd88211b258d16b2c64eeb15f21d4edd9ba
Xiang Liu <xiang.liu@rightcapital.com>
2018-06-21 11:40:01 +0800

    JS debugger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,61 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {COMPILER_OPTIONS, CompilerFactory, createPlatformFactory, enableProdMode, PLATFORM_INITIALIZER} from '@angular/core';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {PlatformRef} from './app/packages/angular/core/src/application_ref';
+import {Console} from './app/packages/angular/core/src/console';
+import {TestabilityRegistry} from './app/packages/angular/core/src/testability/testability';
+import {Injector, StaticProvider} from './app/packages/angular/core/src/di';
+import {PLATFORM_ID} from './app/packages/angular/core/src/application_tokens';
+import {ResourceLoader} from '@angular/compiler';
+import {ResourceLoaderImpl} from './app/packages/angular/platform-browser-dynamic/src/resource_loader/resource_loader_impl';
+import {ɵINTERNAL_BROWSER_PLATFORM_PROVIDERS as INTERNAL_BROWSER_PLATFORM_PROVIDERS} from '@angular/platform-browser';
+import {PlatformLocation, ɵPLATFORM_BROWSER_ID as PLATFORM_BROWSER_ID} from '@angular/common';
+import {JitCompilerFactory} from './app/packages/angular/platform-browser-dynamic/src/compiler_factory';
+import {BrowserPlatformLocation} from './app/packages/angular/platform-browser/src/browser/location/browser_platform_location';
+import {_document, initDomAdapter} from './app/packages/angular/platform-browser/src/browser';
+import {DOCUMENT} from './app/packages/angular/platform-browser/src/dom/dom_tokens';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.log(err));
+
+
+
+
+export const platformCore: ((extraProviders?: StaticProvider[]) => PlatformRef) = createPlatformFactory(null, 'core', [
+  // Set a default platform name for platforms that don't set it explicitly.
+  {provide: PLATFORM_ID, useValue: 'unknown'},
+  {provide: PlatformRef, deps: [Injector]},
+  {provide: TestabilityRegistry, deps: []},
+  {provide: Console, deps: []},
+]);
+
+export const platformCoreDynamic: ((extraProviders?: StaticProvider[]) => PlatformRef) = createPlatformFactory(platformCore, 'coreDynamic', [
+  {provide: COMPILER_OPTIONS, useValue: {}, multi: true},
+  {provide: CompilerFactory, useClass: JitCompilerFactory, deps: [COMPILER_OPTIONS]},
+]);
+
+
+const platformBrowserDynamicTest =createPlatformFactory(platformCoreDynamic, 'browserDynamic', [
+  [
+    {provide: PLATFORM_ID, useValue: PLATFORM_BROWSER_ID},
+    {provide: PLATFORM_INITIALIZER, useValue: initDomAdapter, multi: true},
+    {provide: PlatformLocation, useClass: BrowserPlatformLocation, deps: [DOCUMENT]},
+    {provide: DOCUMENT, useFactory: _document, deps: []},
+  ],
+  {
+    provide: COMPILER_OPTIONS,
+    useValue: {providers: [{provide: ResourceLoader, useClass: ResourceLoaderImpl, deps: []}]},
+    multi: true
+  },
+  {provide: PLATFORM_ID, useValue: PLATFORM_BROWSER_ID},
+]);
+
+const platformRef: PlatformRef = platformBrowserDynamicTest();
+
+console.log(platformRef.injector);
+
