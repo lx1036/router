ca6d5077adcfab4cdf3e5c57703d29fc90101c1f
Xiang Liu <xiang.liu@rightcapital.com>
2019-04-10 12:40:28 +0800

    Fix counter component

--- react/react-demo/src/Components/Counter.tsx
+++ react/react-demo/src/Components/Counter.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { INCREMENT, DECREMENT } from "../redux/actions";
 
-class Counter extends React.Component {
+class Counter extends React.Component<{dispatch: any, count: any}, any> {
   // state = { count: 0 };
 
   increment = () => {
