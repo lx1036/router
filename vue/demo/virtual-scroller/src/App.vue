68a89a84161712a7d949f6b2d94804a12c33bb94
Xiang Liu <xiang.liu@rightcapital.com>
2019-04-19 14:41:22 +0800

    (vue): vue component lifecycle

--- vue/demo/virtual-scroller/src/App.vue
+++ vue/demo/virtual-scroller/src/App.vue
@@ -4,6 +4,39 @@
   </div>
 </template>
 
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component({
+    components: {}
+  })
+  export default class App extends Vue {
+    public beforeCreate() {
+      console.log('beforeCreate');
+    }
+
+    public created() {
+      console.log('created');
+    }
+
+    public beforeMount() {
+      console.log('beforeMount');
+    }
+
+    public mounted() {
+      console.log('mounted');
+    }
+
+    public beforeDestroy() {
+      console.log('beforeDestroy');
+    }
+
+    public destroyed() {
+      console.log('destroy');
+    }
+  }
+</script>
+
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
---
+++ vue/demo/virtual-scroller/src/components/resize-observer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="resize-observer" tabindex="-1"></div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component
+  export class ResizeObserver extends Vue {
+    public name: string = 'resize-observer';
+
+    public mounted() {
+
+    }
+  }
+
+
+  /*export default {
+    name: "resize-observer"
+  }*/
+</script>
+
+<style scoped>
+
+</style>
