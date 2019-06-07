1daec65ce490ddb253ff5e7d15b9584e2c9d7101
Xiang Liu <xiang.liu@rightcapital.com>
2019-06-08 01:22:02 +0800

    (vue3): push/pop/shift/unshift/splice/sort/reverse of array

--- vue/vue3/test/mvvm.spec.js
+++ vue/vue3/test/mvvm.spec.js
@@ -1,4 +1,5 @@
 import {Vue3} from "../src";
+import vnode from "../../vue/src/core/vdom/vnode";
 
 
 describe('mvvm', function () {
@@ -55,5 +56,78 @@ describe('mvvm', function () {
     delete vm.a.b;
     expect(vm.a.b).toEqual(undefined);
     expect(vm.$el.textContent).toEqual('undefined');
+  });
+  
+  it('array getter/setter', () => {
+    const vm = new Vue3({
+      data() {
+        return {a: ['hello']};
+      },
+      render(h) {
+        return h('div', null, this.a[0]);
+      }
+    }).$mount();
+    
+    expect(vm.a[0]).toEqual('hello');
+    expect(vm.$el.textContent).toEqual('hello');
+    vm.a[0] = 'world';
+    expect(vm.a[0]).toEqual('world');
+    expect(vm.$el.textContent).toEqual('world');
+  });
+  
+  it('array push/pop/shift/unshift/splice/sort/reverse', () => {
+    const vm = new Vue3({
+      data() {
+        return {a: ['hello']};
+      },
+      render(h) {
+        return h('div', null, this.a[this.a.length - 1]);
+      }
+    }).$mount();
+  
+    expect(vm.a[0]).toEqual('hello');
+    expect(vm.$el.textContent).toEqual('hello');
+    vm.a.push('world');
+    expect(vm.a[1]).toEqual('world');
+    expect(vm.$el.textContent).toEqual('world');
+    vm.a.pop();
+    expect(vm.a[0]).toEqual('hello');
+    expect(vm.$el.textContent).toEqual('hello');
+    vm.a.shift();
+    expect(vm.a.length).toEqual(0);
+    expect(vm.$el.textContent).toBeNull();
+    vm.a.unshift('hello');
+    expect(vm.a[0]).toEqual('hello');
+    expect(vm.$el.textContent).toEqual('hello');
+    vm.a.splice(0, 1, 'world', 'hello');
+    expect(vm.a[0]).toEqual('world');
+    expect(vm.a[1]).toEqual('hello');
+    expect(vm.$el.textContent).toEqual('hello');
+    vm.a.sort();
+    expect(vm.a[0]).toEqual('hello');
+    expect(vm.a[1]).toEqual('world');
+    expect(vm.$el.textContent).toEqual('world');
+    vm.a.reverse();
+    expect(vm.a[0]).toEqual('world');
+    expect(vm.a[1]).toEqual('hello');
+    expect(vm.$el.textContent).toEqual('hello');
+  });
+  
+  it('setter/getter property of object in array', () => {
+    const vm = new Vue3({
+      data() {
+        return {a: [{msg: 'hello'}]};
+      },
+      render(h) {
+        return h('div', null, this.a[0].msg);
+      }
+    }).$mount();
+    
+    expect(vm.a[0].msg).toEqual('hello');
+    expect(vm.$el.textContent).toEqual('hello');
+    
+    vm.a[0].msg = 'world';
+    expect(vm.a[0].msg).toEqual('world');
+    expect(vm.$el.textContent).toEqual('world');
   });
 });
